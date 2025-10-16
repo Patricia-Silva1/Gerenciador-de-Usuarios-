933e84474367f7788882516e3a8c1414b329faf9
Patrícia Silva <ps397380@gmail.com>
2025-10-15 21:08:15 -0300

    Refactor ListUsers to use API and improve state management

--- src/pages/ListUsers/index.jsx
+++ src/pages/ListUsers/index.jsx
@@ -1,24 +1,71 @@
 import { useEffect, useState } from "react";
+import api from "../../services/api";
+import Button from "../../components/Button";
+import TopBackground from "../../components/TopBackground";
+import Trash from '../../assets/trash.svg';
+import { useNavigate } from "react-router-dom";
+
+import {
+  Container,
+  ContainerUsers,
+  CardUsers,
+  TrashIcon,
+  Title,
+  AvatarUser,
+  BackButton
+} from './styles';
 
 function ListUsers() {
-  const [users, setUsers] = useState([]);
+  const [usuarios, setUsuarios] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    fetch("http://localhost:3001/api/users")
-      .then((res) => res.json())
-      .then(setUsers)
-      .catch((err) => console.error("Erro ao buscar usuários:", err));
+    async function getUsers() {
+      try {
+        const { data } = await api.get("/usuarios");
+        setUsuarios(data);
+      } catch (error) {
+        console.error("Erro ao buscar usuários:", error);
+      }
+    }
+    getUsers();
   }, []);
 
+  async function deleteUsers(id) {
+    try {
+       await api.delete(`/usuarios/${id}`);
+
+// Atualiza a lista após deletar
+const updatedUsers = usuarios.filter((user) => user.id !== id);
+setUsuarios(updatedUsers);
+
+
+    } catch (error) {
+      console.error("Erro ao deletar usuário:", error);
+    }
+  }
+
   return (
-    <div>
-      <h1>Lista de Usuários</h1>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
+    <Container>
+      <TopBackground />
+      <Title>Lista de Usuários</Title>
+
+      <ContainerUsers>
+        {usuarios.map((user) => (
+          <CardUsers key={user.id}>
+            <AvatarUser src={`https://avatar.iran.liara.run/public?username=${user.id}`} />
+            <div>
+              <h3>Nome: {user.name}</h3>
+              <p>Idade: {user.age}</p>
+              <p>Email: {user.email}</p>
+            </div>
+            <TrashIcon src={Trash} alt="icone-lixo" onClick={() => deleteUsers(user.id)} />
+          </CardUsers>
         ))}
-      </ul>
-    </div>
+      </ContainerUsers>
+
+      <BackButton onClick={() => navigate('/')}>← Voltar</BackButton>
+    </Container>
   );
 }
 
